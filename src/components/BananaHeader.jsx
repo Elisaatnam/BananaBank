5de704b589779a0c6843ff2d7dba5cc50268c7d1
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-06-17 20:54:46 +0200

    added assets img

--- src/components/BananaHeader.jsx
+++ src/components/BananaHeader.jsx
@@ -3,7 +3,10 @@ import("./BananaHeader.css");
 const BananaHeader = () => {
 	return (
 		<section className='banana-header'>
-			<img src='./monkeyeatingbanana.png' alt='monkey eating banana' />
+			<img
+				src='../src/assets/img/monkeyeatingbanana.png'
+				alt='monkey eating banana'
+			/>
 			<h1>Banana Bank</h1>
 		</section>
 	);
